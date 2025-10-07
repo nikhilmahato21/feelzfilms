da0fab9118e84df76db1542f4f8a011d57498297
nikhilmahato21 <nikhilmahato3337@gmail.com>
2025-10-07 19:41:06 +0530

    component spacing fix

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -6,7 +6,7 @@ export const Home = () => {
   return (
     <main className=" bg-[#212121] text-white font-sans  ">
       
-      <div className=" md:max-w-[100rem] mx-auto px-2 md:px-6 py-2 md:py-8  space-y-10 md:space-y-20">
+      <div className=" md:max-w-[100rem] mx-auto px-2 md:px-6 py-2 md:py-8  space-y-4 md:space-y-8">
         {/* ====== Components go here ====== */}
         <Hero />
         <VideoEditingSection/>
