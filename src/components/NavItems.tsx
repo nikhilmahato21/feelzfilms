1ff2329e66cad40337253e9696b31ba2108dfb52
nikhilmahato21 <nikhilmahato3337@gmail.com>
2025-10-07 22:15:22 +0530

    menu section style fix

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -28,17 +28,18 @@ export const MobileNav = ({ menuOpen }: MobileNavProps) => {
   if (!menuOpen) return null;
 
   return (
-    <div className="fixed top-[4rem] right-24 z-[99] w-[35%] bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl p-6 shadow-2xl animate-fadeIn">
-      <ul className="flex flex-col gap-2 text-sm font-medium font-unbounded text-center">
-        {navItems.map((item) => (
-          <li
-            key={item.label}
-            className="hover:text-gray-200 cursor-pointer transition"
-          >
-            {item.label}
-          </li>
-        ))}
-      </ul>
-    </div>
+   <div className="fixed top-[4rem] right-32 z-[99] w-[35%] bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl p-6 shadow-2xl animate-scaleIn">
+  <ul className="flex flex-col gap-2 text-sm font-medium font-unbounded text-center">
+    {navItems.map((item) => (
+      <li
+        key={item.label}
+        className="hover:text-gray-200 cursor-pointer transition"
+      >
+        {item.label}
+      </li>
+    ))}
+  </ul>
+</div>
+
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {
 
 export const Navbar: React.FC<NavbarProps> = ({ menuOpen, setMenuOpen }) => {
   return (
-   <nav className="fixed top-3 md:top-10 inset-x-0 mx-auto px-2 md:px-12 flex items-center justify-between py-1 md:py-3 bg-white/10 backdrop-blur-md rounded-full border border-white/20 shadow-sm max-w-[180px] md:max-w-xl lg:max-w-4xl z-50">
+   <nav className="fixed top-3 md:top-10 inset-x-0 mx-auto px-2 md:px-12 flex items-center justify-between py-1 md:py-3 bg-white/10 backdrop-blur-md rounded-full border border-white/20 shadow-sm max-w-[180px] md:max-w-xl lg:max-w-4xl z-50 hover:scale-105 transition-all duration-150">
 
       {/* Logo */}
       <div className="font-unbounded text-sm md:text-lg font-bold tracking-wide">
