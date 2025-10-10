b49506ab9ff7fe5c7318037b94709be9946c93b5
nikhilmahato21 <nikhilmahato3337@gmail.com>
2025-10-11 00:48:50 +0530

    animation bug

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -42,9 +42,9 @@ export const Home = () => {
   return (
     <main className="bg-black text-white font-sans">
       <div className="md:max-w-[100rem] xl:max-w-[150rem] mx-auto py-2 md:py-8 space-y-2 md:space-y-0">
-        <AnimatedSection>
+        
           <Hero />
-        </AnimatedSection>
+        
 
         <AnimatedSection>
           <VisionSection />
