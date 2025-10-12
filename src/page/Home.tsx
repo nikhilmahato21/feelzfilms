e555c7cd362cc79311a05c8cab16e8a2891eb2a2
nikhilmahato21 <nikhilmahato3337@gmail.com>
2025-10-12 23:24:11 +0530

    animations added

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,68 +1,52 @@
-// "use client";
-// import { motion, useInView, type Variants } from "motion/react";
-// import { useRef } from "react";
-
 import Footer from "../components/Footer";
 import { Hero } from "../components/Hero";
 import Portfolio from "../components/Portfolio";
 import TestimonialsSection from "../components/TestimonialsSection";
-
 import { VisionSection } from "../components/VisionSection";
 import VideoEditingSection from "../components/VideoEditingSection";
 import Product from "../components/Product";
 
+import { motion } from "motion/react"; // or 'framer-motion'
+
 export const Home = () => {
-  // Animation variants
-//   const fadeUp: Variants = {
-//   hidden: { opacity: 0, y: 60 },
-//   visible: {
-//     opacity: 1,
-//     y: 0,
-//     transition: { duration: 0.3, ease: "easeOut" },
-//   },
-// };
-
-  // Helper component for scroll animation
-  // const AnimatedSection = ({ children }: { children: React.ReactNode }) => {
-  //   const ref = useRef(null);
-  //  const inView = useInView(ref, { once: false, margin: "-20%" });
-
-
-  //   return (
-  //     <motion.div
-  //       ref={ref}
-  //       variants={fadeUp}
-  //       initial="hidden"
-  //       animate={inView ? "visible" : "hidden"}
-  //     >
-  //       {children}
-  //     </motion.div>
-  //   );
-  // };
+  const fadeUp = {
+    initial: { opacity: 0, y: 50 },
+    whileInView: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+    viewport: { once: true, amount: 0.2 },
+  };
 
   return (
     <main className="bg-black text-white font-sans">
       <div className="md:max-w-[100rem] xl:max-w-[150rem] mx-auto py-2 md:py-8 space-y-2 md:space-y-0">
         
+        <motion.div {...fadeUp}>
           <Hero />
+        </motion.div>
         
+        <motion.div {...fadeUp}>
           <VisionSection />
+        </motion.div>
         
-            <Product/>
+        <motion.div {...fadeUp}>
+          <Product />
+        </motion.div>
         
+        <motion.div {...fadeUp}>
           <VideoEditingSection />
-          
+        </motion.div>
         
-
-        
+        <motion.div {...fadeUp}>
           <Portfolio />
+        </motion.div>
         
-
-        
+        <motion.div {...fadeUp}>
           <TestimonialsSection />
+        </motion.div>
         
-
-        <Footer />
+        <motion.div {...fadeUp}>
+          <Footer />
+        </motion.div>
+        
       </div>
     </main>
   );
